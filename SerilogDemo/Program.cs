6b2b854efa7a11419bd6948d324163f76b8b84fa
Master Yoda <masteryodamaster@outlook.com>
2023-11-16 17:29:20 +0200

    Logging to file

--- SerilogDemo/Program.cs
+++ SerilogDemo/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
 
-Console.WriteLine("Hello, World!");
+namespace SerilogDemo;
+
+public static class Program
+{
+    public static void Main()
+    {
+        // Configure Serilog with different sinks
+
+        // 1. File Sink
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Minute)
+            .CreateLogger();
+        
+        Log.Information("This is an informational message.");
+        Log.Warning("This is a warning message.");
+        Log.Error("This is an error message.");
+        Log.Fatal("This is a fatal message.");
+
+        // Close and flush the log
+        Log.CloseAndFlush();
+    }
+}
